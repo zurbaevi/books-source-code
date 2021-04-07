ae10e40780359789e0b2207de16edd72c9679794
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-04-07 18:47:21 +0400

    Added chapter 15

---
+++ HeadFirstAndroid/starbuzz2/src/main/java/com/hfad/starbuzz2/TopLevelActivity.java
@@ -0,0 +1,30 @@
+package com.hfad.starbuzz2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(onItemClickListener);
+    }
+}

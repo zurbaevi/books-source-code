7d50e597196bf80e262088b2409b789465f48a5c
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-04-07 20:27:39 +0400

    Added chapter 17

---
+++ HeadFirstAndroid/starbuzz4/src/main/java/com/hfad/starbuzz4/DrinkActivity.java
@@ -0,0 +1,103 @@
+package com.hfad.starbuzz4;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrinkActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DRINKID = "drinkId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkId)},
+                    null, null, null);
+            if (cursor.moveToFirst()) {
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
+
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+
+                CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
+            }
+            db.close();
+            cursor.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    //Update the database when the checkbox is clicked
+    public void onFavoriteClicked(View view) {
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+
+        new UpdateDrinkTask().execute(drinkId);
+    }
+
+    private class UpdateDrinkTask extends AsyncTask<Integer, Void, Boolean> {
+        private ContentValues drinkValues;
+
+        protected void onPreExecute() {
+            CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+            drinkValues = new ContentValues();
+            drinkValues.put("FAVORITE", favorite.isChecked());
+        }
+
+        protected Boolean doInBackground(Integer... drinks) {
+            int drinkId = drinks[0];
+            SQLiteOpenHelper starbuzzDatabaseHelper =
+                    new StarbuzzDatabaseHelper(DrinkActivity.this);
+            try {
+                SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+                db.update("DRINK", drinkValues,
+                        "_id = ?", new String[]{Integer.toString(drinkId)});
+                db.close();
+                return true;
+            } catch (SQLiteException e) {
+                return false;
+            }
+        }
+
+        protected void onPostExecute(Boolean success) {
+            if (!success) {
+                Toast toast = Toast.makeText(DrinkActivity.this,
+                        "Database unavailable", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+}

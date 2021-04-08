5de39aeca398037901a46996c881c4f6a1549027
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-04-08 14:45:01 +0400

    Added chapter 18

---
+++ HeadFirstAndroid/joke/src/main/java/com/hfad/joke/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hfad.joke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.response));
+        startService(intent);
+    }
+}

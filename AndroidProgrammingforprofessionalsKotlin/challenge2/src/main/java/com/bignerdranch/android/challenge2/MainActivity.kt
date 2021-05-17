bf8943f18e2ac963de2354dc696e78f0be7aaa99
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-05-17 22:21:04 +0400

    Added chapter 2

--- AndroidProgrammingforprofessionalsKotlin/challenge2/src/main/java/com/bignerdranch/android/challenge2/MainActivity.kt
+++ AndroidProgrammingforprofessionalsKotlin/challenge2/src/main/java/com/bignerdranch/android/challenge2/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bignerdranch.android.geoquiz
+package com.bignerdranch.android.challenge2
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button;
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -42,6 +45,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1 + questionBank.size) % questionBank.size
             updateQuestion()
         }
 
@@ -49,7 +58,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        currentIndex = (currentIndex + 1) % questionBank.size
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }

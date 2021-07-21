9bc91629c6afbfda3d68a42a2ee5e5250a47b663
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-07-21 21:36:39 +0400

    Fixed minor flaw

--- kotlin/android-programming-for-professionals-kotlin/challenge1/src/main/java/com/bignerdranch/android/challenge1/MainActivity.kt
+++ kotlin/android-programming-for-professionals-kotlin/challenge1/src/main/java/com/bignerdranch/android/challenge1/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button;
+    private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
